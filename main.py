c26995ad011d05fb12694d0d3eb34b05d2293f93
kaanvural <kaan.vural.149@gmail.com>
2021-12-13 17:08:47 +0300

    removed punctuations and unnecessary spaces from data

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
+import re
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
-
-
 
 def main():
     df = pd.read_csv("emails.csv")
@@ -24,22 +23,28 @@ def main():
     X_ham = []
     X_spam = []
 
+
     for i in range(len(X_train)):
         if y_train[i] == 1:
             X_spam.append(X_train[i])
         else:
             X_ham.append(X_train[i])
 
-    print(f"X_ham: {X_ham}")
-    print(f"X_spam: {X_spam}")
+    #print(f"X_ham: {X_ham}")
+    #print(f"X_spam: {X_spam}")
 
-    
-
+    X_ham = remove_punctuations(X_ham)
+    X_spam = remove_punctuations(X_spam)
 
     vectorizer = CountVectorizer(ngram_range=(1,1), min_df=0.001, stop_words=ENGLISH_STOP_WORDS)
-
+    print(X_ham[0])
     
 
+def remove_punctuations(data):
+    for i in range(len(data)):
+        data[i] = re.sub(r'[^\w\s]','', data[i])
+        data[i] = re.sub("\s\s+", " ", data[i])
+    return data
 
 
 if __name__ == "__main__":
